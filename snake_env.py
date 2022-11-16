047e313a48fdaa0ba20f08b4cadde7447f8b7cff
Walks2K <walks2000@gmail.com>
2022-11-16 09:16:02 +0000

    Entire rework of snake env
    Works well for "naive" agents but can't plan ahead due to the nature of the obs space.
    
    All game functionality is contained within the env instead of relying on seperate classes.
    
    Read docstring at top for info about action/obs space and reward implementation.

--- snake_env.py
+++ snake_env.py
@@ -1,9 +1,31 @@
 """
-Custom environment for Snake
+Custom environment for Snake using OpenAI Gym
+
+Actions:
+0: left
+1: right
+2: up
+3: down
+
+Observations:
+food above: 0/1 | 0
+food below: 0/1 | 1
+food left: 0/1 | 2
+food right: 0/1 | 3
+obstacle (wall/own body) above: 0/1 | 4
+obstacle (wall/own body) below: 0/1 | 5
+obstacle (wall/own body) left: 0/1 | 6
+obstacle (wall/own body) right: 0/1 | 7
+direction: 0/1/2/3 | 8
+
+Rewards:
+-1: moved away from food
++1: moved towards food
+-10: hit wall/own body
++10: ate food
 """
 
 import random
-from collections import deque
 
 import pygame
 import numpy as np
@@ -12,228 +34,198 @@ from gym import spaces
 import cv2
 
 
-SNAKE_LEN_GOAL = 30
-
-
-class Snake:
-    """
-    Snake class
-    """
-
-    def __init__(self, x, y, length=3):
-        self.x = x
-        self.y = y
-        self.length = length
-        self.start_length = length
-
-        self.direction = "RIGHT"
-        self.body = []
-        self.body.append([self.x, self.y])
-        for i in range(1, self.length):
-            self.body.append([self.x - i, self.y])
-
-    def update(self):
-        """
-        Update snake position
-        """
-        if self.direction == "RIGHT":
-            self.x += 1
-        elif self.direction == "LEFT":
-            self.x -= 1
-        elif self.direction == "UP":
-            self.y -= 1
-        elif self.direction == "DOWN":
-            self.y += 1
-
-        self.body.insert(0, [self.x, self.y])
-        if len(self.body) > self.length:
-            self.body.pop()
-
-    def collide_boundary(self, grid_size):
-        """
-        Check if snake hits the boundary
-        """
-        if self.x >= grid_size[0] or self.x < 0 or self.y >= grid_size[1] or self.y < 0:
-            return True
-        return False
-
-    def collide_snake(self):
-        """
-        Check if snake hits itself
-        """
-        for i in range(1, len(self.body)):
-            if self.x == self.body[i][0] and self.y == self.body[i][1]:
-                return True
-        return False
-
-
-class Food:
-    """
-    Food class
-    """
-
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-
-def get_empty_space(snake, grid_size):
-    """
-    Get empty space on the screen
-    """
-    empty_space = []
-    for x in range(grid_size[0]):
-        for y in range(grid_size[1]):
-            if (x, y) not in snake.body:
-                empty_space.append([x, y])
-    return empty_space
-
-
 class SnakeEnv(gym.Env):
     """
-    Custom Snake environment that follows gym interface
+    Custom Environment that follows gym interface
+
+    Args:
+        width (int): width of the grid
+        height (int): height of the grid
+        fps (int): frames per second
     """
 
-    def __init__(self, grid_size=(15, 15), cell_size=20, fps=30, max_steps=200):
-        """
-        Initialize Snake environment
-        """
+    metadata = {"render.modes": ["human"]}
+
+    def __init__(self, width=10, height=10, fps=10):
         super(SnakeEnv, self).__init__()
-        self.action_space = spaces.Discrete(4)
-        self.observation_space = spaces.Box(
-            low=0, high=1, shape=(16,), dtype=np.float32)
 
-        self.grid_size = grid_size
-        self.cell_size = cell_size
+        self.width = width
+        self.height = height
         self.fps = fps
-        self.max_steps = max_steps
 
-    def step(self, action):
-        """
-        Step through the environment
-        """
-        if action == 0:
-            self.snake.direction = "RIGHT"
-        elif action == 1:
-            self.snake.direction = "LEFT"
-        elif action == 2:
-            self.snake.direction = "UP"
-        elif action == 3:
-            self.snake.direction = "DOWN"
+        self.action_space = spaces.Discrete(4)
+        self.observation_space = spaces.Box(low=0, high=1, shape=(9,), dtype=np.uint8)
 
-        self.snake.update()
+        self.viewer = None
 
-        done = self.snake.collide_boundary(
-            self.grid_size) or self.snake.collide_snake() or self.steps <= 0
-
-        if not done:
-            if self.snake.x == self.food.x and self.snake.y == self.food.y:
-                self.snake.length += 1
-                food_pos = random.choice(
-                    get_empty_space(self.snake, self.grid_size))
-                self.food = Food(food_pos[0], food_pos[1])
-                reward = 10
-                self.steps = self.max_steps
-            else:
-                reward = 0.01
-        else:
-            reward = -10
-
-        self.steps -= 1
-        observation = self.get_observation()
-        return observation, reward, done, {}
+        self.reset()
 
     def reset(self):
-        """
-        Reset/initialize the environment
-        """
-        self.snake = Snake(self.grid_size[0] // 2, self.grid_size[1] // 2)
-        food_pos = random.choice(get_empty_space(self.snake, self.grid_size))
-        self.food = Food(food_pos[0], food_pos[1])
-        self.screen = pygame.display.set_mode(
-            (self.grid_size[0] * self.cell_size, self.grid_size[1] * self.cell_size))
-        pygame.display.set_caption("Snake")
-        pygame.init()
-        pygame.font.init()
-        self.steps = self.max_steps
+        self.snake = [[self.width // 2, self.height // 2]]
+        self.food = self._new_food()
+        self.direction = [0, 0]
+        self.score = 0
+        self.done = False
 
-        observation = self.get_observation()
-        return observation
+        return self._get_obs()
 
-    def render(self):
-        """
-        Render the environment
-        """
+    def step(self, action):
+        self._action(action)
+        self._move()
+        reward, done = self._get_reward()
+        obs = self._get_obs()
+
+        return obs, reward, done, {}
+
+    def render(self, mode="human"):
+        if self.viewer is None:
+            self.viewer = pygame.display.set_mode((self.width * 10, self.height * 10))
+
+        self.viewer.fill((0, 0, 0))
+
+        for x, y in self.snake:
+            pygame.draw.rect(
+                self.viewer,
+                (0, 255, 0),
+                pygame.Rect(x * 10, y * 10, 10, 10),
+            )
+
+        pygame.draw.rect(
+            self.viewer,
+            (255, 0, 0),
+            pygame.Rect(self.food[0] * 10, self.food[1] * 10, 10, 10),
+        )
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+                self.close()
 
-        self.screen.fill((0, 0, 0))
-        for i in range(len(self.snake.body)):
-            if i == 0:
-                pygame.draw.rect(
-                    self.screen, (0, 255, 0), (self.snake.body[i][0] * self.cell_size, self.snake.body[i][1] * self.cell_size, self.cell_size, self.cell_size))
+        pygame.display.flip()
+        pygame.time.Clock().tick(self.fps)
+
+    def close(self):
+        if self.viewer:
+            pygame.display.quit()
+            self.viewer = None
+
+    def _action(self, action):
+        if action == 0:
+            self.direction = [-1, 0]
+        elif action == 1:
+            self.direction = [1, 0]
+        elif action == 2:
+            self.direction = [0, -1]
+        elif action == 3:
+            self.direction = [0, 1]
+
+    def _move(self):
+        new_head = [
+            self.snake[0][0] + self.direction[0],
+            self.snake[0][1] + self.direction[1],
+        ]
+
+        if (
+            new_head[0] < 0
+            or new_head[0] >= self.width
+            or new_head[1] < 0
+            or new_head[1] >= self.height
+        ):
+            self.done = True
+            return
+
+        if new_head in self.snake:
+            self.done = True
+            return
+
+        self.snake.insert(0, new_head)
+
+        if new_head != self.food:
+            self.snake.pop()
+        else:
+            self.food = self._new_food()
+
+    def _new_food(self):
+        while True:
+            food = [random.randrange(self.width), random.randrange(self.height)]
+            if food not in self.snake:
+                return food
+
+    def _get_reward(self):
+        if self.done:
+            return -10, True
+
+        if self.snake[0] == self.food:
+            return 10, False
+
+        if self.direction == [1, 0]:
+            if self.food[0] > self.snake[0][0]:
+                return 1, False
             else:
-                pygame.draw.rect(
-                    self.screen, (255, 255, 255), (self.snake.body[i][0] * self.cell_size, self.snake.body[i][1] * self.cell_size, self.cell_size, self.cell_size))
-        pygame.draw.rect(
-            self.screen, (255, 0, 0), (self.food.x * self.cell_size, self.food.y * self.cell_size, self.cell_size, self.cell_size))
-        pygame.display.update()
+                return -1, False
+        elif self.direction == [-1, 0]:
+            if self.food[0] < self.snake[0][0]:
+                return 1, False
+            else:
+                return -1, False
+        elif self.direction == [0, 1]:
+            if self.food[1] > self.snake[0][1]:
+                return 1, False
+            else:
+                return -1, False
+        elif self.direction == [0, -1]:
+            if self.food[1] < self.snake[0][1]:
+                return 1, False
+            else:
+                return -1, False
 
-    def get_observation(self):
-        """
-        Get observation of environment:
-        Check for danger in each ordinal direction - one hot vector
-        Direction Left, Direction Right, Direction Up, Direction Down - one hot vector
-        Food Left, Food Right, Food Up, Food Down - one hot vector
+    def _get_obs(self):
+        obs = np.zeros(9, dtype=np.uint8)
 
-        Return a numpy array
-        """
+        if self.food[0] < self.snake[0][0]:
+            obs[0] = 1
+        elif self.food[0] > self.snake[0][0]:
+            obs[1] = 1
 
-        # Danger
-        danger_vector = np.zeros(8)
-        if self.snake.y - 1 < 0 or [self.snake.x, self.snake.y - 1] in self.snake.body:  # N
-            danger_vector[0] = 1
-        # NE
-        if self.snake.y - 1 < 0 or self.snake.x + 1 >= self.grid_size[0] or [self.snake.x + 1, self.snake.y - 1] in self.snake.body:
-            danger_vector[1] = 1
-        # E
-        if self.snake.x + 1 >= self.grid_size[0] or [self.snake.x + 1, self.snake.y] in self.snake.body:
-            danger_vector[2] = 1
-        if self.snake.y + 1 >= self.grid_size[1] or self.snake.x + 1 >= self.grid_size[0] or [self.snake.x + 1, self.snake.y + 1] in self.snake.body:  # SE
-            danger_vector[3] = 1
-        # S
-        if self.snake.y + 1 >= self.grid_size[1] or [self.snake.x, self.snake.y + 1] in self.snake.body:
-            danger_vector[4] = 1
-        if self.snake.y + 1 >= self.grid_size[1] or self.snake.x - 1 < 0 or [self.snake.x - 1, self.snake.y + 1] in self.snake.body:  # SW
-            danger_vector[5] = 1
-        if self.snake.x - 1 < 0 or [self.snake.x - 1, self.snake.y] in self.snake.body:  # W
-            danger_vector[6] = 1
-        # NW
-        if self.snake.y - 1 < 0 or self.snake.x - 1 < 0 or [self.snake.x - 1, self.snake.y - 1] in self.snake.body:
-            danger_vector[7] = 1
+        if self.food[1] < self.snake[0][1]:
+            obs[2] = 1
+        elif self.food[1] > self.snake[0][1]:
+            obs[3] = 1
 
-        # Direction
-        direction_vector = np.zeros(4)
-        if self.snake.direction == "RIGHT":
-            direction_vector[0] = 1
-        elif self.snake.direction == "LEFT":
-            direction_vector[1] = 1
-        elif self.snake.direction == "UP":
-            direction_vector[2] = 1
-        elif self.snake.direction == "DOWN":
-            direction_vector[3] = 1
+        if self.snake[0][0] == 0 or [self.snake[0][0] - 1, self.snake[0][1]] in self.snake:
+            obs[4] = 1
+        if (
+            self.snake[0][0] == self.width - 1
+            or [self.snake[0][0] + 1, self.snake[0][1]] in self.snake
+        ):
+            obs[5] = 1
+        if self.snake[0][1] == 0 or [self.snake[0][0], self.snake[0][1] - 1] in self.snake:
+            obs[6] = 1
+        if (
+            self.snake[0][1] == self.height - 1
+            or [self.snake[0][0], self.snake[0][1] + 1] in self.snake
+        ):
+            obs[7] = 1
 
-        # Food
-        food_vector = np.zeros(4)
-        if self.food.x < self.snake.x:
-            food_vector[0] = 1
-        elif self.food.x > self.snake.x:
-            food_vector[1] = 1
-        if self.food.y < self.snake.y:
-            food_vector[2] = 1
-        elif self.food.y > self.snake.y:
-            food_vector[3] = 1
+        if self.direction == [-1, 0]:
+            obs[8] = 0
+        elif self.direction == [1, 0]:
+            obs[8] = 1
+        elif self.direction == [0, -1]:
+            obs[8] = 2
+        elif self.direction == [0, 1]:
+            obs[8] = 3
 
-        return np.concatenate((danger_vector, direction_vector, food_vector))
+        return obs
+
+    def _get_image(self):
+        img = np.zeros((self.width * 10, self.height * 10, 3), dtype=np.uint8)
+        for x, y in self.snake:
+            img[x * 10 : x * 10 + 10, y * 10 : y * 10 + 10] = (0, 255, 0)
+        img[
+            self.food[0] * 10 : self.food[0] * 10 + 10,
+            self.food[1] * 10 : self.food[1] * 10 + 10,
+        ] = (255, 0, 0)
+        return img
+
+    def get_image(self):
+        return cv2.cvtColor(self._get_image(), cv2.COLOR_RGB2BGR)
